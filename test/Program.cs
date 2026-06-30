6152ca247e733f3872bf89ae842ea01ec552c82e
berre <bernhard.vm@gmail.com>
2026-06-30 15:37:32 +0200

    add packages

--- test/Program.cs
+++ test/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, Vilvoorde and Belgium and Europe!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ModelContextProtocol.Server;
+
+var builder = Host.CreateEmptyApplicationBuilder(settings: null);
+
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport();
+
+await builder.Build().RunAsync();
